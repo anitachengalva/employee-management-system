58c39ccbba3cc4c5163748462cc7426b77d9df7c
Anita Chengalva <anita.chengalva@gmail.com>
2022-05-12 20:10:13 -0700

    set MySQL and schema.sql

---
+++ schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+    id INT PRIMARY KEY,
+    name VARCHAR(30),
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments (id)
+    ON DELETE SET NULL,
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee (id)
+    ON DELETE SET NULL
+);
+
