5ef8f0258c794691794e8e3ca22ea6c5357e0719
Anita Chengalva <anita.chengalva@gmail.com>
2022-05-13 12:59:09 -0700

    added const and .js files for add/update/view

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // requires npms
 const inquirer = require("inquirer");
 const mySQL = require("mysql2");
+const table = require("console.table");
+
+// add, update, and view
+const add = require("./lib/add");
+const update = require("./lib/update");
+const view = require("./lib/view"); 
 
 // create a link between database and .js
 const db = mySQL.createConnection(
