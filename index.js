cdd101688ba67313bbfd26ec3b108eca9797a8d5
Anita Chengalva <anita.chengalva@gmail.com>
2022-05-13 17:01:28 -0700

    got rid of unnecessary files, consolidating js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const inquirer = require("inquirer");
 const mySQL = require("mysql2");
 const table = require("console.table");
 
-// add, update, and view
-const add = require("./lib/add");
-const update = require("./lib/update");
-const view = require("./lib/view"); 
 const Connection = require("mysql2/typings/mysql/lib/Connection");
 
 // create a link between database and .js
