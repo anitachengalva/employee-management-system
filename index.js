66c5311ef9351efa2344764b188845581b21c77d
Anita Chengalva <anita.chengalva@gmail.com>
2022-05-14 12:52:39 -0700

    fixed bug, inquirer launches in terminal

--- index.js
+++ index.js
@@ -3,26 +3,32 @@ const inquirer = require("inquirer");
 const mySQL = require("mysql2");
 const table = require("console.table");
 
-const Connection = require("mysql2/typings/mysql/lib/Connection");
-
 // create a link to database
-const db = mySQL.createConnection(
+const connection = mySQL.createConnection(
     {
         host: "localhost",
+        port: "3300",
         user: "root",
         password: "mysql",
         database: "employee_db",
     }
 );
 
+// check connection
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected at " + connection.threadId);
+    welcome();
+});
+
 // welcome message
-function init() {
-    console.log("Welcome to the Employee Database")
+function welcome() {
+    console.log("Welcome to the Employee Database");
     userChoices();
 }
 
 // inquirer prompt
-function userChoices(){
+function userChoices() {
 inquirer
     .prompt([
         {
@@ -66,6 +72,7 @@ inquirer
         }
     ])
 
+    // returns promise
     // how the code responds to each possible user input choice
     .then(({ startOptions }) => {
         if(startOptions === "viewAllDepartments") {
@@ -98,17 +105,43 @@ inquirer
 
 // VIEW functions
     // view all departments
+    function viewAllDepartments() {
+        console.log("Viewing all Departments");
+        connection.query("SELECT * FROM departments", function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            nowDone();
+        })
+    }
 
     // view all roles
+    function viewAllRoles() {
+        console.log("Viewing all Roles");
+    }
 
     // view all employees
+    function viewAllEmployees() {
+        console.log("Viewing all Employees")
+    }
 
 // ADD functions
     // add department
+    function addDepartment() {
+
+    }
 
     // add role
+    function addRole() {
+
+    }
 
     // add employee
+    function addEmployee() {
+
+    }
 
 // UPDATE functions
     // update role
+    function updateRole() {
+
+    }
